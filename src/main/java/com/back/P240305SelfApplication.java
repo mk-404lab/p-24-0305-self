e48e8117bd4a66ea9f52d5e1fedde30211860af5
mk-404lab <alsgur9984@naver.com>
2026-03-10 17:08:18 +0900

    init

---
+++ src/main/java/com/back/P240305SelfApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P240305SelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P240305SelfApplication.class, args);
+    }
+
+}
