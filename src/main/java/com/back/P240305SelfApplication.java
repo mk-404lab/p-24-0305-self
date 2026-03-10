ff569a08dcc7f0206cfc1c374776249022dd5b09
mk-404lab <alsgur9984@naver.com>
2026-03-10 17:30:37 +0900

    feat: 1~2강 - 초기 셋팅, 샘플 데이터 3개 생성

--- src/main/java/com/back/P240305SelfApplication.java
+++ src/main/java/com/back/P240305SelfApplication.java
@@ -2,8 +2,10 @@ package com.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class P240305SelfApplication {
 
     public static void main(String[] args) {
