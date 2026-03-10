470b4f9e3de28fca43107c73dc916682610e80b5
mk-404lab <alsgur9984@naver.com>
2026-03-10 18:11:14 +0900

    feat: 7강 - 유효성 체크 구현

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -18,6 +18,37 @@ public class PostController {
     @GetMapping("/posts/write-form")
     @ResponseBody
     public String writeForm() {
+        return getWriteForm();
+    }
+
+
+    @PostMapping("/posts/write")
+    @ResponseBody
+    public String write(String title, String content) {
+
+        //유효성 체크
+        if (title.isBlank()) {
+            return """
+                    <div style="color:red">제목을 입력해주세요.</div>
+                    %s
+                    """.formatted(getWriteForm());
+
+        }
+
+        if (content.isBlank()) {
+            return """
+                    <div style="color:red">내용을 입력해주세요.</div>
+                    %s
+                    """.formatted(getWriteForm());
+        }
+
+        Post post = postService.write(title, content);
+
+        return "%d번 글이 작성되었습니다.".formatted(post.getId());
+    }
+
+
+    private String getWriteForm() {
         return """
                 <form method="post" action="/posts/write">
                   <input type="text" name="title">
@@ -27,14 +58,5 @@ public class PostController {
                   <input type="submit" value="작성">
                 </form>
                 """;
-    }
-
-
-    @PostMapping("/posts/write")
-    @ResponseBody
-    public String write(String title, String content) {
-        Post post = postService.write(title, content);
-
-        return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
 }
